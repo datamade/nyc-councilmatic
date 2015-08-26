6264b3dbe2674a7e5b33ebb1e1001e18e76ac758
Cathy Deng <cathyd125@gmail.com>
2015-08-26 14:09:08 -0500

    shorten overly wordy action labels

--- nyc/templatetags/nyc_extras.py
+++ nyc/templatetags/nyc_extras.py
@@ -25,6 +25,11 @@ def remove_action_subj(bill_action_desc):
 	clean_action = re.sub(r'\bComm\b', 'Committee', bill_action_desc)
 	clean_action = re.sub(r'\bRecved\b', 'Received', clean_action)
 	clean_action = re.sub(r'[,\s]*by\s[^\s]*', '', clean_action)
+
+	# shorten the really long action descriptions for approval w/ modifications
+	if 'approved with modifications' in clean_action.lower():
+		clean_action = 'Approved with Modifications'
+
 	return clean_action
 
 @register.filter
