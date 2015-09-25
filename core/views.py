2c6c60c4fcce8c8b606acea7bc5e1466898e2c1a
Cathy Deng <cathyd125@gmail.com>
2015-09-25 14:55:32 -0500

    fix home page

--- core/views.py
+++ core/views.py
@@ -32,7 +32,7 @@ def city_context(request):
 def index(request):
     one_month_ago = date.today() + timedelta(days=-30)
     recent_legislation = Bill.objects.exclude(last_action_date=None).filter(last_action_date__gt=one_month_ago).order_by('-last_action_date').all()
-    recently_passed = [l for l in recent_legislation if l.inferred_status == 'Passed']
+    recently_passed = [l for l in recent_legislation if l.inferred_status == 'Passed' and l.bill_type == 'Introduction']
 
     context = {
         'recent_legislation': recent_legislation,
