8b7918fe0a9f114e492bf9b041650e33afd0ab3e
Cathy Deng <cathyd125@gmail.com>
2015-09-28 10:53:09 -0500

    update descriptions - remove local laws, re-word stuff for clarity

--- councilmatic/city_config.py
+++ councilmatic/city_config.py
@@ -19,52 +19,56 @@ LEGISLATION_TYPE_DESCRIPTIONS = [
     {
         'name': 'Introduction',
         'search_term': 'Introduction',
-        'desc': 'A proposal for a NYC local law. \
-                An introduction becomes a local law if (1) both the Council & Mayor approve, \
-                (2) the Mayor vetoes but the Council achieves 2/3 vote within 30 days of the veto, or\
-                (3) the Council approves and the mayor does not act within 30 days.',
+        'html_desc': True,
+        'desc': 'A proposal for a NYC local law. <br/>\
+                Ways for an introduction to become a local law: <ul><li>the Council approves & the Mayor approves,</li> \
+                <li>the Mayor vetoes & the Council achieves 2/3 vote within 30 days, or</li>\
+                <li>the Council approves & the Mayor does not act within 30 days</li></ul>',
 
     },
     {
         'name': 'Resolution',
         'search_term': 'Resolution',
-        'desc': "An expression of the Council's sentiment on important public policy issues that may or may not fall under City jurisdiction. \
-                Resolutions are often used to support federal or state legislation, commemorate people and events, or even criticize policies. \
-                Resolutions can also be used to adopt budget and land use items.",
+        'html_desc': True,
+        'desc': "An expression of the Council's opinion on public policy issues that may or may not fall under City jurisdiction. \
+                Resolutions can be used to: <ul><li>support/oppose federal or state legislation,</li>\
+                <li>commemorate people and events, or</li>\
+                <li>adopt budget and land use items</li></ul>",
     },
     {
         'name': 'SLR (State Legislation Resolution)',
         'search_term': 'SLR',
+        'html_desc': False,
         'desc': 'A special resolution that serves as an official home rule request from the Council to the State Senate and State Assembly to pass pending legislation in the New York State Legislature. The initial request for a SLR must come from the Legislature in Albany.',
     },
     {
         'name': 'Land Use Application',
         'search_term': 'Land Use Application',
+        'html_desc': False,
         'desc': 'Land use proposals to be considered by the Council’s Land Use Committee',
     },
     {
         'name': 'Land Use Call-Up',
         'search_term': 'Land Use Call-Up',
+        'html_desc': False,
         'desc': "Land use proposals that don't require Council approval, but have been chosen by a Council Member for discretionary Council review.",
-    },
-    {
-        'name': 'Local Law 2015',
-        'search_term': 'Local Laws 2015',
-        'desc': 'All NYC laws that have been enacted for the year 2015.',
     },
     {
         'name': 'Mayor’s Messages',
         'search_term': "Mayor's Message",
+        'html_desc': False,
         'desc': 'Communications from the Mayor. These generally include appointments to boards and commissions, as well as veto messages.',
     },
     {
         'name': 'Communications',
         'search_term': 'Communication',
+        'html_desc': False,
         'desc': 'Communications from the City, County or Borough offices. These usually concern appointments to boards and commissions.',
     },
     {
         'name': 'Petitions',
         'search_term': 'Petition',
+        'html_desc': False,
         'desc': 'Communication from individuals or entities other than the Mayor, City, County or Borough Offices. These generally include communications from the City Council, and usually concern City Council appointments and resignations.',
     },
 ]
