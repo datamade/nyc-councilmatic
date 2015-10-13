b59f2c47005af3e0275efaab18e10ace27f2c116
Cathy Deng <cathyd125@gmail.com>
2015-10-13 11:40:45 -0500

    excluding invalid nyc bill types from index

--- nyc/search_indexes.py
+++ nyc/search_indexes.py
@@ -6,3 +6,8 @@ class NYCBillIndex(BillIndex, indexes.Indexable):
     
     def get_model(self):
         return NYCBill
+
+    def index_queryset(self, using=None):
+        # excluding NYC bill types that are not really legislation
+        invalid_bill_types = ['Town Hall Meeting', 'Oversight', 'Tour', 'Local Laws 2015']
+        return self.get_model().objects.exclude(bill_type__in=invalid_bill_types)
