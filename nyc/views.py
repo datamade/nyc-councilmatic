b6150ed5880ce3ad6908dfbe4586ba2bcdaa66f9
Cathy Deng <cathyd125@gmail.com>
2016-05-16 16:37:19 -0500

    adding popular pages from google analytics

--- nyc/views.py
+++ nyc/views.py
@@ -1,12 +1,53 @@
 from django.shortcuts import render
 from datetime import date, timedelta
 from nyc.models import NYCBill
-from councilmatic_core.models import Event, Organization
+from councilmatic_core.models import Event, Organization, Person
 from councilmatic_core.views import *
+import googleanalytics as ga
+
 
 class NYCIndexView(IndexView):
     template_name = 'nyc/index.html'
     bill_model = NYCBill
+
+    def extra_context(self):
+        extra_context = {}
+
+        extra_context['analytics'] = self.get_analytics_data()
+
+        return extra_context
+
+    def get_analytics_data(self):
+
+        profile = ga.authenticate(identity='nyc-councilmatic',
+                interactive=True,
+                save=True,
+                account='Councilmatic',
+                webproperty='NYC Councilmatic',
+                profile='NYC Councilmatic')
+
+        month_views = profile.core.query.metrics('pageviews')\
+                            .dimensions('pagepath')\
+                            .monthly(months=-1)\
+                            .sort('pageviews', descending=True)
+
+        top_bills = []
+        top_people = []
+        for date, page, count in month_views.rows:
+            if len(top_people)<5 and page.startswith('/person/'):
+                slug = page.split('/')[2]
+                top_people.append(Person.objects.get(slug=slug))
+            if len(top_bills)<5 and page.startswith('/legislation/'):
+                slug = page.split('/')[2]
+                top_bills.append(self.bill_model.objects.get(slug=slug))
+
+        analytics_blob = {
+            'top_bills': top_bills,
+            'top_people': top_people,
+        }
+
+        return analytics_blob
+
 
 class NYCAboutView(AboutView):
     template_name = 'nyc/about.html'
