82cd966448ed022ee1cfe60eeead531f3babf771
reginafcompton <reginafcompton@gmail.com>
2016-11-21 10:50:19 -0600

    Refine search functionality for date

--- nyc/search_indexes.py
+++ nyc/search_indexes.py
@@ -1,9 +1,18 @@
-from councilmatic_core.haystack_indexes import BillIndex
+
 from haystack import indexes
+
+from django.conf import settings
+
+from datetime import datetime, timedelta
+import pytz
+
 from nyc.models import NYCBill
+from councilmatic_core.haystack_indexes import BillIndex
+from councilmatic_core.models import Action
+
 
 class NYCBillIndex(BillIndex, indexes.Indexable):
-    
+
     def get_model(self):
         return NYCBill
 
@@ -11,3 +20,17 @@ class NYCBillIndex(BillIndex, indexes.Indexable):
         # excluding NYC bill types that are not really legislation
         invalid_bill_types = ['Town Hall Meeting', 'Oversight', 'Tour', 'Local Laws 2015']
         return self.get_model().objects.exclude(bill_type__in=invalid_bill_types)
+
+    def prepare_last_action_date(self, obj):
+        app_timezone = pytz.timezone(settings.TIME_ZONE)
+
+        if not obj.last_action_date:
+            index_actions = [a.date for a in obj.actions.all()]
+
+            if index_actions:
+                index_actions = max(index_actions)
+
+            print(index_actions)
+            return index_actions
+
+        return obj.last_action_date
