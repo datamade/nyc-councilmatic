3374255f2c075d5fa91378988395a80ce7f69901
reginafcompton <reginafcompton@gmail.com>
2016-11-22 10:32:45 -0600

    Fix sort by

--- nyc/views.py
+++ nyc/views.py
@@ -82,9 +82,9 @@ class NYCCouncilmaticFacetedSearchView(CouncilmaticFacetedSearchView):
                     if 'title' in el:
                         try:
                             dataDict['descending']
-                            kwargs['searchqueryset'] = sqs.order_by('-friendly_name')
+                            kwargs['searchqueryset'] = sqs.order_by('-sort_name')
                         except:
-                            kwargs['searchqueryset'] = sqs.order_by('friendly_name')
+                            kwargs['searchqueryset'] = sqs.order_by('sort_name')
                     if 'relevance' in el:
                         kwargs['searchqueryset'] = sqs
 
