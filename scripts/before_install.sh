18426e5681cb341e6c5e17ce7dc69b90904098af
reginafcompton <reginafcompton@gmail.com>
2017-06-05 12:15:52 -0500

    Add before install

---
+++ scripts/before_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Make project directory if it doesn't exist. This is mainly to ensure that these scripts work on a bare server
+
+rm -Rf /home/datamade/nyc
+mkdir -p /home/datamade/nyc
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "staging" ]
+then
+    rm -Rf /home/datamade/nyc-staging
+    mkdir -p /home/datamade/nyc-staging
+fi
+if [ "$DEPLOYMENT_GROUP_NAME" == "production" ]
+then
+    rm -Rf /home/datamade/nyc-councilmatic
+    mkdir -p /home/datamade/nyc-councilmatic
+fi
+
+# Decrypt blackbox-encrypted files 
+cd /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/ && chown -R datamade.datamade . && sudo -H -u datamade blackbox_postdeploy
